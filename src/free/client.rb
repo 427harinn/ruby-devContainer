80a84982b671997b91fef6d8748737a92dfedcc7
Haruka Karakawa <rinnrenn424@yahoo.co.jp>
2024-07-08 16:39:39 +0900

    シンプルにjsonを取得して表示はできてる

--- src/free/client.rb
+++ src/free/client.rb
@@ -1,11 +1,46 @@
 require 'socket'
+require 'json'
 
-TCPSocket.open("localhost",5000) do |s|
+# サーバーに接続
+TCPSocket.open("localhost", 5000) do |s|
+  # リクエストを送信
+  s.print "GET /data1 HTTP/1.0\r\n"
+  s.print "\r\n"
 
-    s.print "GET /non HTTP/1.0\r\n"
-    s.print "\r\n"
+  # レスポンスを受信
+  response = ""
+  while line = s.gets
+    response += line
+  end
 
-    while line=s.gets
-        puts line
+  # デバッグ用にレスポンス全体をエスケープシーケンスを含む形で表示
+  #puts "Response received:"
+  #puts response.inspect
+
+  # ヘッダーとボディを分離
+  headers, body = response.split("\n\n")
+
+
+  # ボディが nil でないことを確認
+  if body.nil?
+    puts "Error: Response body is nil"
+  else
+    # ボディをJSONとして解析
+    begin
+      data = JSON.parse(body)
+      
+      # JSONデータに応じた内容をコンソールに表示
+      if data["time"]
+        puts "Current Time: #{data["time"]}"
+      elsif data["name"] && data["email"]
+        puts "ID: #{data["id"]}, Name: #{data["name"]}, Email: #{data["email"]}"
+      elsif data["name"]
+        puts "Name: #{data["name"]}"
+      else
+        puts "Error: #{data["error"]}"
+      end
+    rescue JSON::ParserError => e
+      puts "Error parsing JSON: #{e.message}"
     end
+  end
 end
--- src/free/server.rb
+++ src/free/server.rb
@@ -1,39 +1,52 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 
 require 'socket'
+require 'json'
 
+# サーバーがリスニングするポートを設定
 gs = TCPServer.open(5000)
 
-
+# 無限ループでクライアントからの接続を待ち受け
 while true
-    s1 = gs.accept
-    Thread.new(s1) do |s|
-        cmd,path,*dummy = s.gets.split(" ")
-        while line = s.gets
-            line.chomp!
-            break if line == ""
-        end
+  s1 = gs.accept
+  Thread.new(s1) do |s|
+    # リクエストの最初の行を取得
+    request_line = s.gets
+    if request_line
+      cmd, path, *dummy = request_line.split(" ")
+      while line = s.gets
+        line.chomp!
+        break if line == ""
+      end
 
-        
-        #s.puts "HTTP/1.1 200 OK"
-        #s.puts path
-        #s.puts cmd
+      # HTTPレスポンスの基本情報を準備
+      s.puts "HTTP/1.1 200 OK"
+      s.puts "Content-Type: application/json"
+      s.puts
 
-        if path == "/now"
-          s.puts "HTTP/1.1 200 OK"
-          s.puts
-          s.puts Time.now
-        elsif path == "/name"
-          s.puts "HTTP/1.1 200 OK"
-          s.puts
-          s.puts "KyushuSangyoUniversity"
+      # パスに応じたJSONデータを送信
+      case path
+      when "/data1"
+        if File.exist?("free/data1.json")
+          s.puts File.read("free/data1.json")
         else
-          s.puts "HTTP/1.1 404 NotFound"
-          s.puts
-          s.puts "404 NotFound"
+          s.puts({ error: "data1.json not found" }.to_json)
         end
-        
-        s.close
+      when "/data2"
+        if File.exist?("free/data2.json")
+          s.puts File.read("free/data2.json")
+        else
+          s.puts({ error: "data2.json not found" }.to_json)
+        end
+      else
+        s.puts "HTTP/1.1 404 Not Found"
+        s.puts "Content-Type: application/json"
+        s.puts
+        s.puts({ error: "404 Not Found" }.to_json)
+      end
     end
+
+    s.close
+  end
 end
 gs.close
